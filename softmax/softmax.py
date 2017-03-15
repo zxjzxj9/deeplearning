00cafa50191b16a501f53aacabb93a2364ec80a1
Victor (Xiao-Jie) Zhang <zxjzxj9@gmail.com>
2017-03-15 17:46:56 +0800

    [devel] Add some code

--- softmax/softmax.py
+++ softmax/softmax.py
@@ -10,8 +10,20 @@ class TFSoftMax(object):
         self.data = trainData
         self.label = trainLabel
 
-    def train(self):
-        pass
+    def train(self, batchsize = 100):
+        #pass
+        wsize = self.data.shape[1]
+        bsize = self.label.shape[1]
+
+        W = tf.Variables(np.random.randn(wsize, bszie))
+        b = tf.Variables(np.random.randn(bsize))
+
+        x = tf.placeholder(tf.float32, [None, wsize])
+        y = tf.nn.softmax(matmul(x*w) + b)
+        y_ = tf.placeholder(tf.float32, [None, bsize])
+
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(
+
 
 if __name__ == "__main__":
     
